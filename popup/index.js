df2ca9af1dc800feb0e3492ed557eb35621e0867
Jordan Porter <silentsummitsoftware@gmail.com>
2021-05-24 16:38:51 -0700

    clean up

--- popup/index.js
+++ popup/index.js
@@ -1,6 +1,5 @@
 const getRandomImage = () => {
     const xhttp = new XMLHttpRequest();
-    // xhttp.open("GET", "https://api.giphy.com/v1/gifs/search?api_key=oWskRqAliCwuyzc6QAJPOBcf9Xxxa2K0&q=cheeseburger&rating=pg", true);
     xhttp.open("GET", "https://api.giphy.com/v1/gifs/random?api_key=oWskRqAliCwuyzc6QAJPOBcf9Xxxa2K0&rating=g", true);
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -11,8 +10,6 @@ const getRandomImage = () => {
     };
     xhttp.send();
 }
-
-
 
 setInterval(() => getRandomImage(), 5000);
 getRandomImage();
